67f31c0b54080c8b2d1a37275b472552c4c985a4
Muhammad Hussain <ksaad7933@gmail.com>
2024-02-13 17:58:30 +0500

    tuesday commit

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const fs= require("fs");
 const url=require("url")
 
 const myServer = http.createServer((req, res) =>{
-    console.log(req.headers)
-    res.end("Hello From Server");
+    const log= `${Date.now()}: ${req.url}: New Req Recieved\n`
+    if(req.url== "/favicon.ico") return res.end()
+    fs.appendFile('log.txt',log,(err,data)=>{
+        switch(req.url){
+            case "/":
+                res.end("home page")
+            break;
+            case "/about":
+            res.end("about page")
+            break;
+            default:
+                res.end("page not found")
+        }
+    })
+   
 })
 myServer.listen(8000,()=>console.log("Server Started"))
