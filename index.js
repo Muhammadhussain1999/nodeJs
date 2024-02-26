4ff4de1c9baff5a3bef7edde22ecdd8e597f2c82
Muhammad Hussain <ksaad7933@gmail.com>
2024-02-26 17:11:02 +0500

    Monday Commit

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const http= require("http");
-const fs= require("fs");
-const url=require("url")
 
-const myServer = http.createServer((req, res) =>{
-    const log= `${Date.now()}: ${req.url}: New Req Recieved\n`
-    if(req.url== "/favicon.ico") return res.end()
-    fs.appendFile('log.txt',log,(err,data)=>{
-        switch(req.url){
-            case "/":
-                res.end("home page")
-            break;
-            case "/about":
-            res.end("about page")
-            break;
-            default:
-                res.end("page not found")
-        }
-    })
-   
+const express= require('express');
+const users=require('./MOCK_DATA.json')
+const app=express()
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users)
 })
-myServer.listen(8000,()=>console.log("Server Started"))
+
+
+app.listen(2000,()=>console.log("Server Started"))
